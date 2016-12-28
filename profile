a7d13d50a85e4b45aa8a7a917ace7e8c40022b3b
Minsub Sim <minsubsim@gmail.com>
2016-12-28 18:29:16 +0900

    syd version

--- profile
+++ profile
@@ -1,51 +1,11 @@
-# if running bash
-if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
-    if [ -f "$HOME/.bashrc" ]; then
-        . "$HOME/.bashrc"
-    fi
-fi
+
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 if [ -f ~/.git-completion.bash ]; then
-    . ~/.git-completion.bash
-fi
-
-# add ~/.env to PATH
-if [ -d "$HOME/.env" ] ; then
-    export PATH="`find -L ~/.env -maxdepth 2 -type d -name bin | tr '\n' ':'`${PATH}"
-    export MANPATH="`find -L ~/.env -maxdepth 4 -type d -name man | tr '\n' ':'`${MANPATH}"
+  . ~/.git-completion.bash
 fi
 
 # Bash - Vim mode
 set -o vi
 
-# aliases
-alias rm="rm -i"
-alias ls="ls --color=auto"
-alias ll="ls -l"
-
-# jcurl, require Pygments(sudo pip install Pygments)
-function jcurl {
-    curl "$@" | python -mjson.tool | pygmentize -l json
-}
-
-# Auto Env
-function cd {
-    builtin cd "$@"
-    if [ -n "$AUTO_ENV" ] && [ "$(expr `pwd` : $AUTO_ENV)" -eq "0"  ]; then
-        deactivate
-        unset AUTO_ENV
-    else
-        if [ -f ./bin/activate ]; then
-            export AUTO_ENV=`pwd`
-            source ./bin/activate
-        fi
-    fi
-}
-
-# Go
-export GOPATH=~/.env/go
-export PATH=$GOPATH/bin:$PATH
-
-# Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
--- setup.sh
+++ setup.sh
@@ -2,37 +2,23 @@
 cd "$(dirname $0)"
 CWD="$(pwd)"
 
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 gpg --keyserver hkp://keys.gnupg.net \
     --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
-if [[ $(uname -a) =~ "Darwin" ]] && ! [[ $(which ln) =~ "/.env/coreutils" ]]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install $(cat brew_requirements)
-    brew cask install $(cat brew_cask_requirements)
-    mkdir -p $HOME/.env/coreutils
-    ln -sf /usr/local/opt/coreutils/libexec/gnubin $HOME/.env/coreutils/bin
-    ln -sf /usr/local/opt/coreutils/libexec/gnuman $HOME/.env/coreutils/man
-else
-    sudo apt-get install -y git vim python-pip
-    sudo ln -sf $CWD/limits.conf /etc/security/limits.conf
-fi
+
+brew install $(cat brew_requirements)
+brew cask install $(cat brew_cask_requirements)
+
 ln -sf $CWD/profile $HOME/.profile
 source $HOME/.profile
 
 ln -sf $CWD/vimrc $HOME/.vimrc
 ln -sf $CWD/gitconfig $HOME/.gitconfig
-ln -sf $CWD/gitignore_global $HOME/.gitignore_global
-ln -sf $CWD/pryrc $HOME/.pryrc
-ln -sf $CWD/rubocop.yml $HOME/.rubocop.yml
 
 git submodule update --init
-ln -sfT $CWD/vim $HOME/.vim
+ln -sf $CWD/vim/bundle/Vundle.vim $HOME/.vim/bundle/Vundle.vim
+
 vim +PluginInstall +qall
 vim +PluginClean! +qall
-
-# golang
-vim +GoInstallBinaries +qall
-
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-
-sudo pip install -r pip_requirements
